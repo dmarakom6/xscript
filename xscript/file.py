4a3837d1ee7cfa8bc69372fa63dfe09822be2948
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-04-03 06:54:26 +0800

    Add files via upload

--- xscript/file.py
+++ xscript/file.py
@@ -9,8 +9,5 @@ def fileflush(fileobj):
 def fileopen(name, mode='r'):
     return open(name, mode)
 
-def fileread(fileobj, size=-1):
-    return fileobj.read(size)
-
 def filewrite(fileobj, s):
     return fileobj.write(s)
--- xscript/ui.py
+++ xscript/ui.py
@@ -1,4 +1,4 @@
-# xscript/ui.py
+# xSheets/xscript/ui.py
 
 import tkinter as _tk
 import tkinter.ttk as _ttk
@@ -7,23 +7,8 @@ from tkinter import messagebox as _messagebox
 def askyesno(message='', title='xscript'):
     return _messagebox.askyesno(message=message, title=title)
 
-def Button(master):
-    return _ttk.Button(master)
-
-def configure(widget, item, value):
-    widget[item] = value
-
 def destroy(widget):
     widget.destroy()
-
-def Label(master):
-    return _ttk.Label(master)
-
-def mainloop(widget):
-    widget.mainloop()
-
-def pack(widget):
-    widget.pack(widget)
 
 def showinfo(message='', title='xscript'):
     return _messagebox.showinfo(message=message, title=title)
@@ -31,5 +16,5 @@ def showinfo(message='', title='xscript'):
 def title(widget, title=''):
     return widget.title(title)
 
-def Window():
+def window():
     return _tk.Tk()
