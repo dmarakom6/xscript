191ee1a2fa8c8df3f6a154cc8bf27e05d68e7610
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-04-07 21:01:02 +0800

    Add files via upload

--- xscript.py
+++ xscript.py
@@ -5,16 +5,19 @@ import os.path as path
 import sys
 
 def main():
-	if sys.argv > 2:
-		print('Error: Too many arguments')
-		exit(1)
-	elif not path.isfile(sys.argv[1]):
-		print('Error: No such file or directory: %s' % sys.argv[1])
-		exit(1)
-	else:
-		source = open(sys.argv[1], 'r+').read()
-		interpreter = core.XScriptInterpreter(source)
-		interpreter.run()
+    if len(sys.argv) > 2:
+        print('Error: Too many arguments')
+        exit(1)
+    elif len(sys.argv) <= 1:
+        print('Error: xscript takes 1 xscript file argument')
+        exit(1)
+    elif not path.isfile(sys.argv[1]):
+        print('Error: No such file or directory: %s' % sys.argv[1])
+        exit(1)
+    else:
+        source = open(sys.argv[1], 'r+').read()
+        interpreter = core.XScriptInterpreter(source)
+        interpreter.run()
 
 if __name__ == '__main__':
-	main()
+    main()
