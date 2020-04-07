971bd358d594d760ad66ccfc81862346cbe76c54
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-04-07 20:40:06 +0800

    Update xscript.py

--- xscript.py
+++ xscript.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 
 import core
+import os.path as path
 import sys
+
+def main():
+	if sys.argv > 2:
+		print('Error: Too many arguments')
+		exit(1)
+	elif not path.isfile(sys.argv[1]):
+		print('Error: No such file or directory: %s' % sys.argv[1])
+		exit(1)
+	else:
+		source = open(sys.argv[1], 'r+').read()
+		interpreter = core.XScriptInterpreter(source)
+		interpreter.run()
+
+if __name__ == '__main__':
+	main()
