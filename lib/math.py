8ddf0b25352fd2ac0b79211203ca5ae5b1eb42e4
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-04-29 08:41:51 +0800

    Update math.py

--- lib/math.py
+++ lib/math.py
@@ -1,4 +1,15 @@
+# xscript/lib/math.py
+
 import math
 
 def abs(x):
-    return abs(x)
+    return abs(float(x))
+
+def cos(x):
+    return cos(float(x))
+
+def sin(x):
+    return sin(float(x))
+
+def tan(x):
+    return tan(float(x))
