0e6507cddaaf86ae4a3d782aec4b31a16e04bb67
Jason Zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-06-13 08:35:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ from distutils.core import setup
 
 setup(
     name='xscript',
-    version='2020-6-12',
-    author='jason-bowen-zheng',
-    author_email='13239073174@wo.cn',
+    version='2020-6-13',
     packages=['xscriptlib'],
     py_modules=['xscriptcore']
 )
