88d02f4aec3ca5b74b6bc082e98d8b3f0ed548c7
jason-bowen-zheng <13239073174@wo.cn>
2020-06-14 16:43:26 +0800

    2020-06-14

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 # xscript/setup.py
 
 from distutils.core import setup
+from time import strftime
 
 setup(
     name='xscript',
-    version='2020-6-14',
+    version=strftime('%Y-%m-%d'),
     license='GPLv3',
     packages=['xscriptlib'],
     py_modules=['xscriptcore']
